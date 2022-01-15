40236539224866965bb4239dab44eca0c40e28fa
Tomasz Świstak <swistak.tomasz@outlook.com>
2022-01-15 20:35:02 +0100

    changed benchmarked values

--- generators-benchmark.js
+++ generators-benchmark.js
@@ -6,9 +6,10 @@ const functions = [
     require("./generators/sundaram"),
 ];
 
-const limits = Array.from(Array(26).keys()).map((x) => 2 ** x);
+const limits = Array.from(Array(1000).keys()).map((x) => 100 * (x + 1));
+limits.unshift(1, 2, 10, 25, 50);
 
-const result = benchmark(functions, limits, 20);
+const result = benchmark(functions, limits, 50);
 const csv = result.map((x) => x.join(";")).join("\n");
 
 fs.writeFileSync("./generators-benchmark.csv", csv, "utf-8");
